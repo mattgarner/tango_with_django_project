702a5f5ece96633e9af224802b74f9e0fb7765b5
mattgarner <matt.j.garner@gmail.com>
2017-01-03 16:24:02 +0000

    Initial commit

---
+++ rango/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rango.models import Category, Page
+
+# Create your views here.
+
+def index(request):
+    page_list = Page.objects.order_by("-views")[:5]
+    category_list = Category.objects.order_by("-likes")[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'name': "Matt"}
+    return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request, 'rango/category.html', context_dict)
